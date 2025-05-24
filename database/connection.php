94ebfcfef1ceca8557549d1f42ebe32610ae43ee
Msira Martine <67377977+Msira-cyber@users.noreply.github.com>
2025-05-24 19:43:56 +0300

    Update connection.php
    Setting Database connection

--- database/connection.php
+++ database/connection.php
@@ -1,14 +1,14 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "agriloan";
+DEFINE('HOSTNAME', 'localhost');
+DEFINE('USERNAME', 'root');
+DEFINE('PASSWORD', ''); 
+DEFINE('DATABASE', 'agriloan');
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+function db_agriloan_connect()
+{
+    $conn = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE) or die('CONNECTION TO SonaTapV4 DATABASE FAILED!');
+    return $conn;
 }
+?>
