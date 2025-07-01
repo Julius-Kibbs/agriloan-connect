b0e5bf9c5083e1af8c40a857c11fb7b00c40591b
Msira-cyber <67377977+Msira-cyber@users.noreply.github.com>
2025-07-01 11:41:18 +0300

    Update loan_review.php

--- UI/pages/loan_review.php
+++ UI/pages/loan_review.php
@@ -155,7 +155,7 @@ $stmt->close();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-    <title>Admin Loans | Agri-Loan Connect</title>
+    <title>Loans Review | Agri-Loan Connect</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-icons.css" rel="stylesheet" />
     <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-svg.css" rel="stylesheet" />
