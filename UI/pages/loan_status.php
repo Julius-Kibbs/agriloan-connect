2f7c7d5283511ae9d1e83e3f9195162258566a71
Msira-cyber <67377977+Msira-cyber@users.noreply.github.com>
2025-07-01 11:41:52 +0300

    Update loan_status.php

--- UI/pages/loan_status.php
+++ UI/pages/loan_status.php
@@ -70,7 +70,7 @@ if (isset($_GET['download_csv']) && !empty($schedules)) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-    <title>Dashboard | Agri-Loan Connect</title>
+    <title>Loan Status | Agri-Loan Connect</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-icons.css" rel="stylesheet" />
     <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-svg.css" rel="stylesheet" />
